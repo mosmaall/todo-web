47a4ed327da82a51d3f8613afa0fd5a7dff82985
sarin udompanit <mos_sarin@hotmail.com>
2019-07-11 01:36:03 +0700

    feat: Styled TodoForm component

--- src/TodoApp/TodoForm/index.js
+++ src/TodoApp/TodoForm/index.js
@@ -1,5 +1,22 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { space, fontSizes, palette } from '../../theme'
+
+const Input = styled.input`
+  width: 100%;
+  padding: ${space.md};
+  font-size: ${fontSizes.md};
+  outline: none;
+  border: none;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  color: ${palette.primary};
+  font-weight: bold;
+
+  ::placeholder {
+    color: ${palette.grey20};
+  }
+`
 
 const TodoForm = props => {
   const { handleAddTask } = props
@@ -17,7 +34,7 @@ const TodoForm = props => {
 
   return (
     <form onSubmit={onSubmit} data-testid="todo-form">
-      <input
+      <Input
         data-testid="todo-input"
         placeholder="Add task"
         value={value}
