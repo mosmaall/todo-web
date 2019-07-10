00d7eb33a1a90162ced1d6f7d7539bf32b7fd368
sarin udompanit <mos_sarin@hotmail.com>
2019-07-11 01:35:53 +0700

    feat: Add theme style sheet

---
+++ src/theme/index.js
@@ -0,0 +1,35 @@
+export const space = {
+  xxxs: '1px',
+  xxs: '2px',
+  xs: '5px',
+  sm: '10px',
+  md: '15px',
+  lg: '20px',
+  xl: '25px',
+  xxl: '30px',
+  xxxl: '50px',
+}
+
+export const fontSizes = {
+  xxxxs: '0.625rem', // 10 px
+  xxxs: '0.75rem', // 12 px
+  xxs: '0.8125rem', // 13 px
+  xs: '0.875rem', // 14 px
+  sm: '0.9375rem', // 15 px
+  md: '1rem', // 16 px
+  lg: '1.125rem', // 18 px
+  xl: '1.25rem', // 20 px
+  xxl: '1.5rem', // 24 px
+  xxxl: '2.25rem', // 36 px
+}
+
+export const palette = {
+  grey90: 'rgba(0, 0, 0, 0.9)',
+  grey80: 'rgba(0, 0, 0, 0.8)',
+  grey50: 'rgba(0, 0, 0, 0.5)',
+  grey40: 'rgba(0, 0, 0, 0.4)',
+  grey20: 'rgba(0, 0, 0, 0.2)',
+  grey10: 'rgba(0, 0, 0, 0.1)',
+  grey5: 'rgba(0, 0, 0, 0.05)',
+  primary: '#F3AC67',
+}
