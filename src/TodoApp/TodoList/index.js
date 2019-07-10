2c3a2f4fd8e2fd6528133ef01e3d09638c227b07
sarin udompanit <mos_sarin@hotmail.com>
2019-07-11 01:58:33 +0700

    feat: Styled todolist component

--- src/TodoApp/TodoList/index.js
+++ src/TodoApp/TodoList/index.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import { space } from '../../theme'
+
+const Container = styled.div`
+  margin-top: ${space.md};
+
+  ul {
+    list-style-type: none;
+    padding-inline-start: 0;
+
+    li {
+      margin-bottom: ${space.md};
+      padding: ${space.md};
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    }
+  }
+`
 
 const TodoList = props => {
   const { todos } = props
@@ -11,9 +28,9 @@ const TodoList = props => {
   ))
 
   return (
-    <div data-testid="todo-list">
+    <Container data-testid="todo-list">
       {todos.length > 0 ? <ul>{itemList}</ul> : <>Empty</>}
-    </div>
+    </Container>
   )
 }
 
