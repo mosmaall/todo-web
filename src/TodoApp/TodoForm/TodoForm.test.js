d5c5233dbc72831c93d4063f5e25c4f8f7009490
sarin udompanit <mos_sarin@hotmail.com>
2019-07-10 23:17:00 +0700

    feat: Test TodoForm can change  input value

--- src/TodoApp/TodoForm/TodoForm.test.js
+++ src/TodoApp/TodoForm/TodoForm.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import TodoForm from '.'
+import '@testing-library/jest-dom/extend-expect'
 
 afterEach(cleanup)
 
@@ -8,5 +9,17 @@ describe('TodoForm', () => {
   it('has input', () => {
     const { getByPlaceholderText } = render(<TodoForm />)
     getByPlaceholderText('Add task')
+  })
+  it('can change input value', () => {
+    const { getByPlaceholderText } = render(<TodoForm />)
+    const input = getByPlaceholderText('Add task')
+    console.log('input')
+    fireEvent.change(input, {
+      target: {
+        value: 'NEW TASK',
+      },
+    })
+
+    expect(input).toHaveAttribute('value', 'NEW TASK')
   })
 })
