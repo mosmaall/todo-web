bb63d719811f309dfe68f7e012f1663e9e909de3
sarin udompanit <mos_sarin@hotmail.com>
2019-07-10 23:33:32 +0700

    feat: Test calls handleAddTask with value when submit

--- src/TodoApp/TodoForm/TodoForm.test.js
+++ src/TodoApp/TodoForm/TodoForm.test.js
@@ -21,4 +21,22 @@ describe('TodoForm', () => {
 
     expect(input).toHaveAttribute('value', 'NEW TASK')
   })
+  it('calls "handleAddTask" with value when press enter ', () => {
+    const handleAddTask = jest.fn()
+    const { getByPlaceholderText } = render(
+      <TodoForm handleAddTask={handleAddTask} />
+    )
+
+    const input = getByPlaceholderText('Add task')
+    fireEvent.change(input, {
+      target: {
+        value: 'NEW TASK',
+      },
+    })
+    fireEvent.keyDown(input, {
+      key: 'Enter',
+      code: 13,
+    })
+    expect(handleAddTask).toBeCalledWith('NEW TASK')
+  })
 })
