0cea06992a206d24ccc41039d4d9bf3fb5385c89
sarin udompanit <mos_sarin@hotmail.com>
2019-07-15 04:46:32 +0700

    feat: Test ProgressBar has style width equal 0 percent

--- src/TodoApp/Navbar/ProgressBar/ProgressBar.test.js
+++ src/TodoApp/Navbar/ProgressBar/ProgressBar.test.js
@@ -6,8 +6,25 @@ import '@testing-library/jest-dom/extend-expect'
 afterEach(cleanup)
 
 describe('ProgressBar', () => {
+  const todos = [
+    {
+      id: 'todo-1',
+      title: 'TODO-1',
+      isFinished: false,
+    },
+    {
+      id: 'todo-2',
+      title: 'TODO-2',
+      isFinished: false,
+    },
+  ]
+
   it('render progress bar', () => {
     const { getByTestId } = render(<ProgressBar />)
     expect(getByTestId('progress-bar')).toBeInTheDocument()
+  })
+  it('has style width equal 0%', () => {
+    const { getByTestId } = render(<ProgressBar percent={0} />)
+    expect(getByTestId('progress-bar')).toHaveStyle('width: 0%')
   })
 })
--- src/TodoApp/Navbar/ProgressBar/index.js
+++ src/TodoApp/Navbar/ProgressBar/index.js
@@ -2,12 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const Container = styled.div``
+const Container = styled.div`
+  width: 200px;
+  height: 50px;
+  background: black;
+`
+
+const Progress = styled.div`
+  height: inherint;
+  width: ${props => props.width || 0};
+  background: white;
+`
 
 const ProgressBar = props => {
-  return <Container data-testid="progress-bar">ProgressBar</Container>
+  const { percent } = props
+  return (
+    <Container>
+      <Progress data-testid="progress-bar" width={`${percent}%`} />
+    </Container>
+  )
 }
 
-ProgressBar.propTypes = {}
+ProgressBar.propTypes = {
+  percent: PropTypes.number,
+}
 
 export default ProgressBar
