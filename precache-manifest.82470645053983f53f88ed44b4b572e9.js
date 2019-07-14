1fbaa7bfabbd5564c6be1f2acd1f8fde27954547
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-14 22:23:57 +0000

    Deploy mosmaall/todo-web to github.com/mosmaall/todo-web.git:gh-pages

--- precache-manifest.82470645053983f53f88ed44b4b572e9.js
+++ precache-manifest.82470645053983f53f88ed44b4b572e9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "462dcd54615c96ac946e88cb1b7d0e43",
+    "revision": "0e8dcb39404018bc2843630d54cfc402",
     "url": "./index.html"
   },
   {
-    "revision": "328258c23b54b23fdf6d",
+    "revision": "cd5b49ebdaf495c749dc",
     "url": "./static/css/main.d2b879cb.chunk.css"
   },
   {
-    "revision": "24891c05c552ff7799da",
-    "url": "./static/js/2.6a420082.chunk.js"
+    "revision": "58af5a2dca77e0a8b4d6",
+    "url": "./static/js/2.1fd5952c.chunk.js"
   },
   {
-    "revision": "328258c23b54b23fdf6d",
-    "url": "./static/js/main.771a1ac0.chunk.js"
+    "revision": "cd5b49ebdaf495c749dc",
+    "url": "./static/js/main.b308f02d.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7502dfc6c485cd5fe4583e06f5b1b598.js"
+  "./precache-manifest.82470645053983f53f88ed44b4b572e9.js"
 );
 
 self.addEventListener('message', (event) => {
