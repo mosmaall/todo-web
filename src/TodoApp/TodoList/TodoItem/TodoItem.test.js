c55e7e93e1e6402b3bc78c5d1455185201e7113c
sarin udompanit <mos_sarin@hotmail.com>
2019-07-15 01:04:51 +0700

    feat: Test TodoItem call handleDelete with id when click remove btn

--- src/TodoApp/TodoList/TodoItem/TodoItem.test.js
+++ src/TodoApp/TodoList/TodoItem/TodoItem.test.js
@@ -7,8 +7,28 @@ import TodoItem from '.'
 afterEach(cleanup)
 
 describe('TodoItem', () => {
+  const todo = {
+    id: 'todo-1',
+    title: 'Have a lunch',
+  }
+
   it('should render title text', () => {
-    const { getByTestId } = render(<TodoItem title="Have a lunch" />)
+    const { getByTestId } = render(<TodoItem title={todo.title} />)
     expect(getByTestId('todo-item')).toHaveTextContent('Have a lunch')
+  })
+  it('call "handleDelete" with id when click remove button', () => {
+    const handleRemoveTodo = jest.fn()
+    const { getByTestId } = render(
+      <TodoItem
+        title={todo.title}
+        id={todo.id}
+        handleRemoveTodo={handleRemoveTodo}
+      />
+    )
+    const delButton = getByTestId('remove-btn')
+
+    fireEvent.click(delButton)
+
+    expect(handleRemoveTodo).toHaveBeenCalledWith(todo.id)
   })
 })
--- src/TodoApp/TodoList/TodoItem/index.js
+++ src/TodoApp/TodoList/TodoItem/index.js
@@ -2,12 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const TodoItem = props => {
-  const { title } = props
-  return <div data-testid="todo-item">{title}</div>
+  const { title, id, handleRemoveTodo } = props
+  return (
+    <div data-testid="todo-item">
+      {title}
+      <button data-testid="remove-btn" onClick={() => handleRemoveTodo(id)}>
+        Remove
+      </button>
+    </div>
+  )
 }
 
 TodoItem.propTypes = {
   title: PropTypes.string,
+  id: PropTypes.string,
 }
 
 export default TodoItem
