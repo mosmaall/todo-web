5faf7e05e309994be7a91e32639e37c20ae3ae55
sarin udompanit <mos_sarin@hotmail.com>
2019-07-15 23:53:34 +0700

    fix: Change todo e2e test

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -56,9 +56,9 @@ describe('todo app', () => {
 
   it('As a user i can edit todo title', () => {
     cy.get('[data-testid=todo-input]').type('Have a lunch{enter}')
-    cy.get('[data-testid=todo-item] span').click()
+    cy.get('[data-testid=todo-item] p').click()
     cy.get('[data-testid=edit-input]').clear()
     cy.get('[data-testid=edit-input]').type('Have a wife{enter}')
-    cy.get('[data-testid=todo-item] span').contains('Have a wife')
+    cy.get('[data-testid=todo-item] p').contains('Have a wife')
   })
 })
